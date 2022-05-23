faa9d1de8dfb4993c7442af3af13d6d25ce0ecd0
Коля Зименков <k.zimenkov@mail.ru>
2022-05-23 10:02:45 +0300

    ...

---
+++ ZeroGram02/User_Info.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ZeroGram02
+{
+    /// <summary>
+    /// Логика взаимодействия для User_Info.xaml
+    /// </summary>
+    public partial class User_Info : Window
+    {
+        public User_Info()
+        {
+            InitializeComponent();
+        }
+    }
+}
