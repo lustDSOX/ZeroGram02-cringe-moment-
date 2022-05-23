ded32b83a0a118961c1fa3a594d1c05c4f68bba0
laimishDOTSOX <asvaltgovno@gmail.com>
2022-05-23 09:50:31 +0300

    Добавьте файлы проекта.

---
+++ ZeroGram02/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ZeroGram02
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ZeroGramMain zeroGramMain = new ZeroGramMain();
+            zeroGramMain.Show();
+        }
+
+        private void login_text_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void login_text_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (login_text.Text == "Login") login_text.Text = "";
+        }
+
+        private void login_text_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (login_text.Text == "") login_text.Text = "Login";
+        }
+
+        private void password_text_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (password_text.Text == "Password") password_text.Text = "";
+        }
+
+        private void password_text_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (password_text.Text == "") password_text.Text = "Password";
+        }
+    }
+}
