2f0aa14a81716a88be3bd1ec592936d6c2d1e977
laimishDOTSOX <asvaltgovno@gmail.com>
2022-05-25 20:06:37 +0300

    dds

--- ZeroGram02/User_Info.xaml.cs
+++ ZeroGram02/User_Info.xaml.cs
@@ -83,7 +83,11 @@ namespace ZeroGram02
                     string[] data_array = line.Split(';');
                     if (int.Parse(data_array[0]) == ID)
                     {
-                        string writingLine = data_array[0] + ";" + Login.Text + ";" + CurrentPassword.Text + ";" + data_array[3] + ";" + data_array[4];
+                        string writingLine = data_array[0] + ";" + Login.Text + ";" + CurrentPassword.Text;
+                        for (int i = 3; i < data_array.Length; i++)
+                        {
+                            writingLine += ";" + data_array[i];
+                        }
                         sw.WriteLine(writingLine);
                     }
                     else
@@ -97,17 +101,29 @@ namespace ZeroGram02
 
         private void Login_LostFocus(object sender, RoutedEventArgs e)
         {
+            if (Login.Text == "") Login.Text = "Login";
             Login.IsReadOnly = true;
         }
 
         private void CurrentPassword_LostFocus(object sender, RoutedEventArgs e)
         {
+            if (CurrentPassword.Text == "") CurrentPassword.Text = "Password";
             CurrentPassword.IsReadOnly = true;
         }
 
         private void Back_Click(object sender, RoutedEventArgs e)
         {
             mainWindow.OpenPage(MainWindow.pages.maininterface, ID);
+        }
+
+        private void CurrentPassword_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (CurrentPassword.Text == "Password") CurrentPassword.Text = "";
+        }
+
+        private void Login_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (Login.Text == "Login") Login.Text = "";
         }
     }
 }
